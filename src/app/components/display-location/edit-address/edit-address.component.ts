37e497b6778a9151da633d01151ccc1fa9ff96eb
Niels Thykier <nit@globeteam.com>
2023-03-30 13:05:18 +0200

    Provide more features for edit-address component
    Signed-off-by: Niels Thykier <nit@globeteam.com>

--- src/app/components/display-location/edit-address/edit-address.component.ts
+++ src/app/components/display-location/edit-address/edit-address.component.ts
@@ -2,6 +2,17 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
 import {ControlContainer, NgModelGroup} from '@angular/forms';
 import {Address} from '../../../../../projects/bkg-swagger-client';
 
+export interface RequiredAddressFields {
+  name?: boolean;
+  street?: boolean;
+  streetNumber?: boolean;
+  floor?: boolean;
+  postCode?: boolean;
+  city?: boolean;
+  stateRegion?: boolean;
+  country?: boolean;
+}
+
 @Component({
   selector: 'app-edit-address',
   templateUrl: './edit-address.component.html',
@@ -25,8 +36,24 @@ export class EditAddressComponent {
   @Input()
   required = true;
 
-  hasAddress = false;
+  _forceAddress = false;
 
+  @Input()
+  set forceAddress(newValue: boolean) {
+    this._forceAddress = newValue;
+    if (!this.hasAddress) {
+      if (!this.address) {
+        this.onAddressSlideToggleChange();
+      } else {
+        this.hasAddress = true;
+      }
+    }
+  }
+
+  @Input()
+  requiredFields?: RequiredAddressFields;
+
+  hasAddress = false;
 
   onAddressSlideToggleChange(): void {
     this.hasAddress = !this.hasAddress;
