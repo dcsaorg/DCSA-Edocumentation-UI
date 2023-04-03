835e43848ab7b4ed13e29f87cde3f7d0685cc310
Niels Thykier <nit@globeteam.com>
2023-04-03 17:55:37 +0200

    Remove URL from partyContactDetails
    Signed-off-by: Niels Thykier <nit@globeteam.com>

--- projects/bkg-swagger-client/model/partyContactDetail.ts
+++ projects/bkg-swagger-client/model/partyContactDetail.ts
@@ -1,6 +1,6 @@
 /**
  * DCSA OpenAPI specification for Booking v2 - Beta 1
- * API specification issued by DCSA.org.  For explanation to specific values or objects please refer to the *** [Information Model v2022.1]() *** Will be updated soon ***. This API does not define the business rules regarding what is allowed to update at what time. For this the [BKG IFS]() *** Will be updated soon *** should be consulted.  All other documents related to the Booking publication can be found [here](https://knowledge.dcsa.org/s/publication?publicationId=a0r7T000000L8mmQAC)  It is possible to use this API as a standalone API. In order to do so it is necessary to use the poll-endPoint - /v2/events  in order to poll event information.  It is recomended to implement the [DCSA Documentation Event Hub](https://app.swaggerhub.com/apis/dcsaorg/DOCUMENTATION_EVENT_HUB) in order to use the push model. Here events are pushed as they occur.  For a changelog please click [here](https://github.com/dcsaorg/DCSA-OpenAPI/tree/master/bkg/v2#v200B1). Please also [create a GitHub issue](https://github.com/dcsaorg/DCSA-OpenAPI/issues/new) if you have any questions/comments. 
+ * API specification issued by DCSA.org.  For explanation to specific values or objects please refer to the *** [Information Model v2022.1]() *** Will be updated soon ***. This API does not define the business rules regarding what is allowed to update at what time. For this the [BKG IFS]() *** Will be updated soon *** should be consulted.  All other documents related to the Booking publication can be found [here](https://knowledge.dcsa.org/s/publication?publicationId=a0r7T000000L8mmQAC)  It is possible to use this API as a standalone API. In order to do so it is necessary to use the poll-endPoint - /v2/events  in order to poll event information.  It is recomended to implement the [DCSA Documentation Event Hub](https://app.swaggerhub.com/apis/dcsaorg/DOCUMENTATION_EVENT_HUB) in order to use the push model. Here events are pushed as they occur.  For a changelog please click [here](https://github.com/dcsaorg/DCSA-OpenAPI/tree/master/bkg/v2#v200B1). Please also [create a GitHub issue](https://github.com/dcsaorg/DCSA-OpenAPI/issues/new) if you have any questions/comments.
  *
  * The version of the OpenAPI document: 2.0.0-Beta-1
  * Contact: info@dcsa.org
@@ -12,9 +12,9 @@
 
 
 /**
- * Contact information for a Party 
+ * Contact information for a Party
  */
-export interface PartyContactDetail { 
+export interface PartyContactDetail {
     /**
      * Name of the contact
      */
@@ -27,9 +27,5 @@ export interface PartyContactDetail {
      * E-mail address for the contact
      */
     email?: string;
-    /**
-     * URL for the contact
-     */
-    url?: string;
 }
 
